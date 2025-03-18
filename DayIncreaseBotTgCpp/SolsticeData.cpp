e6a1416693840c962d733f0adb1e96ed46270497
alya <ellliotros@gmail.com>
2025-03-18 08:22:49 +0100

    Fix the data format and the solsticeData map

--- DayIncreaseBotTgCpp/SolsticeData.cpp
+++ DayIncreaseBotTgCpp/SolsticeData.cpp
@@ -5,15 +5,31 @@
 #include <ostream>
 #include <sstream>
 
+namespace
+{
+    std::chrono::system_clock::time_point createDate(int year, int month, int day, int hour, int minute)
+    {
+        std::tm timeInfo = {};
+        timeInfo.tm_year = year - 1900;
+        timeInfo.tm_mon = month - 1;
+        timeInfo.tm_mday = day;
+        timeInfo.tm_hour = hour;
+        timeInfo.tm_min = minute;
+        timeInfo.tm_sec = 0;
+
+        return std::chrono::system_clock::from_time_t(std::mktime(&timeInfo));
+    }
+}
+
 std::map<int, std::pair<std::chrono::system_clock::time_point, std::chrono::system_clock::time_point>> SolsticeData::solsticeData =
 {
-    {2024, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(20) + std::chrono::minutes(51), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(9) + std::chrono::minutes(20)}},
-    {2025, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(2) + std::chrono::minutes(42), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(15) + std::chrono::minutes(3)}},
-    {2026, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(8) + std::chrono::minutes(25), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(20) + std::chrono::minutes(50)}},
-    {2027, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(14) + std::chrono::minutes(11), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(2) + std::chrono::minutes(43)}},
-    {2028, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(20) + std::chrono::minutes(2), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(8) + std::chrono::minutes(20)}},
-    {2029, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(1) + std::chrono::minutes(48), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(14) + std::chrono::minutes(14)}},
-    {2030, {std::chrono::system_clock::from_time_t(0) + std::chrono::hours(7) + std::chrono::minutes(31), std::chrono::system_clock::from_time_t(0) + std::chrono::hours(20) + std::chrono::minutes(9)}}
+    {2024, {createDate(2024, 12, 21, 9, 20), createDate(2024, 6, 21, 20, 51)}},
+    {2025, {createDate(2025, 12, 21, 15, 3), createDate(2025, 6, 21, 2, 42)}},
+    {2026, {createDate(2026, 12, 21, 20, 50), createDate(2026, 6, 21, 8, 25)}},
+    {2027, {createDate(2027, 12, 21, 2, 43), createDate(2027, 6, 21, 14, 11)}},
+    {2028, {createDate(2028, 12, 21, 8, 20), createDate(2028, 6, 21, 20, 2)}},
+    {2029, {createDate(2029, 12, 21, 14, 14), createDate(2029, 6, 21, 1, 48)}},
+    {2030, {createDate(2030, 12, 21, 20, 9), createDate(2030, 6, 21, 7, 31)}}
 };
 
 std::optional<std::pair<std::chrono::system_clock::time_point, std::chrono::system_clock::time_point>>
@@ -47,6 +63,6 @@ std::string SolsticeData::formatDate(const std::chrono::system_clock::time_point
     tmInfo = *gmtime(&time);
 
     std::ostringstream oss;
-    oss << std::put_time(&tmInfo, "%d-%m-%Y");
+    oss << std::put_time(&tmInfo, "%Y-%m-%d");
     return oss.str();
 }
