2c04f946246744f50e9547fae24163f74503395f
alya <ellliotros@gmail.com>
2025-03-20 13:35:57 +0100

    Remove thread logic

--- DayIncreaseBotTgCpp/BotManager.cpp
+++ DayIncreaseBotTgCpp/BotManager.cpp
@@ -1,35 +1,44 @@
 ﻿#include "BotManager.h"
 #include <tgbot/tgbot.h>
 #include <iostream>
-#include <memory>
-#include <thread>
-#include <atomic>
 
-BotManager::BotManager(std::shared_ptr<TgBot::Bot> bot, const std::shared_ptr<WeatherApiManager>& weatherApiManager)
+BotManager::BotManager(const std::shared_ptr<TgBot::Bot>& bot,
+                       const std::shared_ptr<WeatherApiManager>& weatherApiManager,
+                       const std::shared_ptr<WeatherDataParser>& weatherDataParser)
     : bot_(bot),
       weatherApiManager_(weatherApiManager),
+      weatherDataParser_(weatherDataParser),
       locationService_(std::make_shared<LocationService>(bot, weatherApiManager_)),
-      updateScheduler_(std::make_shared<UpdateScheduler>(bot, weatherApiManager_)),
-      messageHandler_(std::make_shared<MessageHandler>(weatherApiManager_, locationService_, bot, updateScheduler_)),
-      isRunning(false) {}
+      updateScheduler_(std::make_shared<UpdateScheduler>(bot, weatherApiManager_, weatherDataParser_)),
+      messageHandler_(std::make_shared<MessageHandler>(weatherApiManager_, locationService_, bot, updateScheduler_))
+{
+}
 
 void BotManager::startBot() {
-    std::cout << "Bot is starting..." << '\n';
+    std::cout << "Bot is starting..." << std::endl;
+    StateInfo stateInfo_;
+
     isRunning = true;
+    bot_->getEvents().onAnyMessage([this, stateInfo_](const TgBot::Message::Ptr& message) {
+        const int64_t chatId = message->chat->id;
 
-    std::thread botThread([this]() {
-        try {
-            TgBot::TgLongPoll longPoll(*bot_);
-            while (is_running_) {
-                longPoll.start();
-            }
-        } catch (const std::exception& e) {
-            std::cerr << "Bot encountered an error: " << e.what() << '\n';
+        auto stateDict = messageHandler_->GetStateInfo(chatId);
+        if (stateDict == nullptr) {
+            messageHandler_->SetStateInfo(chatId, stateInfo_);
         }
-    });
 
-    botThread.join();
-    std::cout << "Bot is stopping..." << '\n';
+        messageHandler_->handleUpdate(chatId, message);
+    });
+    try{
+        TgBot::TgLongPoll longPoll(*bot_);
+
+        //it runs in docker, no need to stop explicitly inside the container
+        while (isRunning) {
+            longPoll.start();
+        }
+    } catch (const std::exception& e) {
+        handlePollingError(e);
+    }
 }
 
 void BotManager::stopBot() {
