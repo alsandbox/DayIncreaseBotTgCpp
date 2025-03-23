9648cf918fde4a283825a277fc12336033504bfd
alya <ellliotros@gmail.com>
2025-03-23 14:01:46 +0100

    Add the getIfLocationIsAvailable property

--- DayIncreaseBotTgCpp/LocationService.h
+++ DayIncreaseBotTgCpp/LocationService.h
@@ -3,21 +3,17 @@
 
 #include <tgbot/tgbot.h>
 #include <memory>
-#include <atomic>
 #include "WeatherApiManager.h"
-
 
 class LocationService
 {
 public:
-    void requestLocation(long chatId) const;
-    void handleLocationReceived(long chatId, std::atomic<bool>& cancellation_token, const TgBot::Message::Ptr& message);
     LocationService(const std::shared_ptr<TgBot::Bot>& bot,
                     const std::shared_ptr<WeatherApiManager>& weatherApiManager);
     void requestLocation(int64_t chatId) const;
     void handleLocationReceived(int64_t chatId, const TgBot::Message::Ptr& message);
     std::function<void()> onLocationReceived;
-    
+    bool getIfLocationIsAvailable() const { return isLocationReceived; }
 private:
     std::shared_ptr<TgBot::Bot> bot_;
     std::shared_ptr<WeatherApiManager> weatherApiManager_;
