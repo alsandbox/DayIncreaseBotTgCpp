1fdf030425b021e6830937431f80cb0d68a64846
alya <ellliotros@gmail.com>
2025-03-23 14:29:40 +0100

    Add the UpdateScheduler header

---
+++ DayIncreaseBotTgCpp/UpdateScheduler.h
@@ -0,0 +1,39 @@
+﻿#ifndef UPDATESCHEDULER_H
+#define UPDATESCHEDULER_H
+#include <thread>
+#include <tgbot/Bot.h>
+#include <atomic>
+
+#include "WeatherApiManager.h"
+#include "WeatherDataParser.h"
+
+struct SolsticeStatus
+{
+    bool isSolsticeDay;
+    std::string solsticeType;
+    bool isDaylightIncreasing;
+};
+
+class UpdateScheduler : public std::enable_shared_from_this<UpdateScheduler>
+{
+public:
+    UpdateScheduler(const std::shared_ptr<TgBot::Bot>& bot, const std::shared_ptr<WeatherApiManager>& weatherApiManager,
+                    const std::shared_ptr<WeatherDataParser>& weatherDataParser);
+    void scheduleUvUpdates(const std::function<void()>& handleDaylightInfo);
+    void sendDailyMessage(int64_t chatId);
+    static SolsticeStatus getSolsticeStatus(const std::chrono::system_clock::time_point& currentDate);
+    void handleDaysTillSolstice(int64_t chatId);
+    void cancelUvUpdates();
+    bool getIsDaylightIncreasing() const { return isDaylightIncreasing; }
+    int calculateDaysTillNearestSolstice(const std::chrono::system_clock::time_point today) const;
+
+private:
+    const std::shared_ptr<TgBot::Bot> bot_;
+    std::shared_ptr<WeatherApiManager> weatherApiManager_;
+    std::shared_ptr<WeatherDataParser> weatherDataParser_;
+    std::thread timerThread;
+    std::atomic<bool> stopTimer{false};
+    bool isDaylightIncreasing = false;
+};
+
+#endif
